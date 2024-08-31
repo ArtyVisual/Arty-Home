1e4af896dbdece62b85f1de951a787f0743ff0d1
abbas <abbasvajwana1@gmail.com>
2024-09-01 00:25:07 +0530

    changes

--- Front-end/src/components/T_Finance.jsx
+++ Front-end/src/components/T_Finance.jsx
@@ -22,9 +22,9 @@ const T_Finance = () => {
 
                 <div className='fin1 md:ml-52 fin pt-32 justify-center flex flex-wrap items-center '>
 
-                    <div className='md:w-2/6 h-56 my-8 mx-10 px-5 py-10 box flex justify-between items-center'>
+                    <div className='md:w-2/6 h-56 my-8 mx-10 px-6 py-10 box flex justify-between items-center'>
 
-                        <div className='vertical text-3xl  font-bold text-blue-700'>
+                        <div className='vertical md:text-3xl text-xl  font-bold text-blue-700'>
                             AUGUST
                         </div>
                         <div>
@@ -38,21 +38,21 @@ const T_Finance = () => {
                                     <td>Rent</td>
                                     <td>30000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Maintenance</td>
                                     <td>500</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Electricity Bill</td>
                                     <td>2000</td>
                                     <td>
-                                        <div className='text-center text-red-500 font-semibold text-lg px-2 lobster'>Due</div>
+                                        <div className='text-center text-red-500 font-semibold md:text-lg px-2 lobster'>Due</div>
                                     </td>
                                 </tr>
                             </table>
@@ -61,7 +61,7 @@ const T_Finance = () => {
 
                     <div className='md:w-2/6 h-56 my-8 mx-10 px-5 py-10 box flex justify-between items-center'>
 
-                        <div className='vertical text-3xl  font-bold text-blue-700'>
+                        <div className='vertical md:text-3xl text-xl  font-bold text-blue-700'>
                             JULY
                         </div>
                         <div>
@@ -75,21 +75,21 @@ const T_Finance = () => {
                                     <td>Rent</td>
                                     <td>30000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Maintenance</td>
                                     <td>500</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr className=''>
                                     <td>Electricity Bill</td>
                                     <td>2000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr className=''>
@@ -105,7 +105,7 @@ const T_Finance = () => {
 
                     <div className='md:w-2/6 h-56 my-8 mx-10 px-5 py-10 box flex justify-between items-center'>
 
-                        <div className='vertical text-3xl  font-bold text-blue-700'>
+                        <div className='vertical md:text-3xl text-xl font-bold text-blue-700'>
                             JUNE
                         </div>
                         <div>
@@ -119,21 +119,21 @@ const T_Finance = () => {
                                     <td>Rent</td>
                                     <td>30000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Maintenance</td>
                                     <td>500</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Electricity Bill</td>
                                     <td>1420</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                             </table>
@@ -142,7 +142,7 @@ const T_Finance = () => {
 
                     <div className='md:w-2/6 h-56 my-8 mx-10 px-5 py-10 box flex justify-between items-center'>
 
-                        <div className='vertical text-3xl  font-bold text-blue-700'>
+                        <div className='vertical md:text-3xl text-xl  font-bold text-blue-700'>
                             MAY
                         </div>
                         <div>
@@ -156,21 +156,21 @@ const T_Finance = () => {
                                     <td>Rent</td>
                                     <td>30000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Maintenance</td>
                                     <td>500</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Electricity Bill</td>
                                     <td>1800</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                             </table>
@@ -179,7 +179,7 @@ const T_Finance = () => {
                     
                     <div className='md:w-2/6 h-56 my-8 mx-10 px-5 py-10 box flex justify-between items-center'>
 
-                        <div className='vertical text-3xl  font-bold text-blue-700'>
+                        <div className='vertical md:text-3xl text-xl  font-bold text-blue-700'>
                             APRIL
                         </div>
                         <div>
@@ -193,28 +193,28 @@ const T_Finance = () => {
                                     <td>Rent</td>
                                     <td>30000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Maintenance</td>
                                     <td>500</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Electricity Bill</td>
                                     <td>2200</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Internet BIll</td>
                                     <td>799</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                             </table>
@@ -223,7 +223,7 @@ const T_Finance = () => {
 
                     <div className='md:w-2/6 h-56 my-8 mx-10 px-5 py-10 box flex justify-between items-center'>
 
-                        <div className='vertical text-3xl  font-bold text-blue-700'>
+                        <div className='vertical md:text-3xl text-xlmd:  font-bold text-blue-700'>
                             MARCH
                         </div>
                         <div>
@@ -237,21 +237,21 @@ const T_Finance = () => {
                                     <td>Rent</td>
                                     <td>30000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Maintenance</td>
                                     <td>500</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Electricity Bill</td>
                                     <td>1000</td>
                                     <td>
-                                        <div className='text-center text-green-500 font-semibold text-lg px-2 lobster'>Paid</div>
+                                        <div className='text-center text-green-500 font-semibold md:text-lg px-2 lobster'>Paid</div>
                                     </td>
                                 </tr>
                             </table>
