77e8373382ebe307da14dcd589d07e5c82df3641
abbas <abbasvajwana1@gmail.com>
2024-09-01 13:55:58 +0530

    resp changes

--- Front-end/src/components/T_Doc.jsx
+++ Front-end/src/components/T_Doc.jsx
@@ -5,14 +5,20 @@ import search from './img/search.png';
 import share from './img/share.png';
 
 const T_Doc = () => {
-
-
-   
+    const [documents, setDocuments] = useState([
+        { id: 1, name: 'Lease Agreement 2023-2024' },
+        { id: 2, name: 'Service Report 2024' },
+        { id: 3, name: 'Billing Statement 2024' },
+        { id: 4, name: 'HOA Certificate 2024' }
+    ]);
 
     return (
         <div>
             <div className='back'>
-                <div className='top py-2'>
+                <div style={{ color: '#a27a47' }} className='top1 md:hidden left-1/2 -ml-11 top-5 text-3xl zain'>
+                    DOCUMENTS
+                </div>
+                <div className='hidden md:flex top py-2'>
                     <div className='logo flex justify-start items-center lg:p-4'>
                         <img className='h-8 w-10 mx-2' src={logo} alt="" />
                         <span className='name font-bold text-xl'>Arty-Home</span>
@@ -20,11 +26,10 @@ const T_Doc = () => {
                     <span style={{ color: '#a27a47' }} className='maint zain'>DOCUMENTS</span>
                 </div>
 
-                <div className='ml-52 px-40 pt-32 justify-center flex flex-wrap items-center '>
-
-                    <div className='w-full h-20 my-8 mx-10 px-5 py-10 box flex justify-evenly items-center'>
-
-                            <div>
+                <div className='md:ml-52 md:px-40 pt-32 justify-center flex flex-wrap items-center'>
+                    {documents.map((doc) => (
+                        <div key={doc.id} className='md:w-full w-72 h-20 my-8 md:mx-10 mx-4 px-2 md:py-10 py-2 box flex justify-evenly items-center'>
+                            <div className='hidden md:flex'>
                                 <lord-icon
                                     src="https://cdn.lordicon.com/wzwygmng.json"
                                     trigger="loop"
@@ -34,90 +39,27 @@ const T_Doc = () => {
                                     style={{ width: '50px', height: '50px' }}>
                                 </lord-icon>
                             </div>
-                            <div className='text ntext2'>
-                                Lease Agreement 2023-2024
-                            </div>
-                            <div className='flex justify-center items-center gap-5 cursor-pointer'>
-                                <img src={search} alt="" />
-                                <img src={down} alt="" />   
-                                <img src={share} alt="" />  
- 
-                            </div>
-                            
-                    </div>
-                    <div className='w-full h-20 my-8 mx-10 px-5 py-10 box flex justify-evenly items-center'>
-
-                            <div>
+                            <div className='md:hidden flex'>
                                 <lord-icon
                                     src="https://cdn.lordicon.com/wzwygmng.json"
                                     trigger="loop"
                                     delay="1500"
                                     stroke="bold"
                                     colors="primary:#3080e8,secondary:#a66037"
-                                    style={{ width: '50px', height: '50px' }}>
+                                    style={{ width: '20px', height: '50px' }}>
                                 </lord-icon>
                             </div>
                             <div className='text ntext2'>
-                                Service Report 2024
+                                {doc.name}
                             </div>
                             <div className='flex justify-center items-center gap-5 cursor-pointer'>
                                 <img src={search} alt="" />
-                                <img src={down} alt="" />  
-                                <img src={share} alt="" />  
-
+                                <img src={down} alt="" />
+                                <img src={share} alt="" />
                             </div>
-                            
-                    </div>
-                    <div className='w-full h-20 my-8 mx-10 px-5 py-10 box flex justify-evenly items-center'>
-
-                            <div>
-                                <lord-icon
-                                    src="https://cdn.lordicon.com/wzwygmng.json"
-                                    trigger="loop"
-                                    delay="1500"
-                                    stroke="bold"
-                                    colors="primary:#3080e8,secondary:#a66037"
-                                    style={{ width: '50px', height: '50px' }}>
-                                </lord-icon>
-                            </div>
-                            <div className='text ntext2'>
-                                Billing Statement 2024
-                            </div>
-                            <div className='flex justify-center items-center gap-5 cursor-pointer'>
-                                <img src={search} alt="" />
-                                <img src={down} alt="" />  
-                                <img src={share} alt="" />  
-  
-                            </div>
-                            
-                    </div>
-                    <div className='w-full h-20 my-8 mx-10 px-5 py-10 box flex justify-evenly items-center'>
-
-                            <div>
-                                <lord-icon
-                                    src="https://cdn.lordicon.com/wzwygmng.json"
-                                    trigger="loop"
-                                    delay="1500"
-                                    stroke="bold"
-                                    colors="primary:#3080e8,secondary:#a66037"
-                                    style={{ width: '50px', height: '50px' }}>
-                                </lord-icon>
-                            </div>
-                            <div className='text ntext2'>
-                                HOA certificate 2024
-                            </div>
-                            <div className='flex justify-center items-center gap-5 cursor-pointer'>
-                                <img src={search} alt="" />
-                                <img src={down} alt="" />    
-                                <img src={share} alt="" />  
-
-                            </div>
-                            
-                    </div>
-                    
-                    
+                        </div>
+                    ))}
                 </div>
-                
             </div>
         </div>
     );
