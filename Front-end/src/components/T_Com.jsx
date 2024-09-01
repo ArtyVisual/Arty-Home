570ec07d2e3e9271373218722dcbf5ea55617230
abbas <abbasvajwana1@gmail.com>
2024-09-01 12:39:56 +0530

    changes

--- Front-end/src/components/T_Com.jsx
+++ Front-end/src/components/T_Com.jsx
@@ -18,7 +18,10 @@ const T_Com = () => {
     return (
         <div>
             <div className='back'>
-                <div className='top py-2'>
+                <div style={{ color: '#a27a47' }} className='top1 md:hidden left-1/3 top-5 text-3xl zain'>
+                    COMMUNICATION
+                </div> 
+                <div className='hidden md:flex top py-2'>
                     <div className='logo flex justify-start items-center lg:p-4'>
                         <img className='h-8 w-10 mx-2' src={logo} alt="" />
                         <span className='name font-bold text-xl'>Arty-Home</span>
@@ -26,10 +29,10 @@ const T_Com = () => {
                     <span style={{ color: '#a27a47' }} className='comm zain'>COMMUNICATION</span>
                 </div>
 
-                <div className='ml-52 px-40 pt-32 justify-center flex flex-wrap items-center'>
+                <div className='md:ml-52 px-40 pt-32 justify-center flex flex-wrap items-center'>
                     {broadcasts.length > 0 ? (
                         broadcasts.map((broadcast, index) => (
-                            <div key={index} className='w-full h-fit my-8 mx-10 px-5 py-10 box flex justify-center items-center'>
+                            <div key={index} className='md:w-full h-fit my-8 mx-10 px-5 md:py-10 box md:flex grid justify-items-center justify-center items-center'>
                                 <div className='flex gap-2'>
                                     <div className='grid justify-items-center p-2 gap-2 text-md'>
                                         <span>Date :</span>                                
