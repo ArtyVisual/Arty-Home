f846f978bf546410726406f164ab64b198251bc1
abbas <abbasvajwana1@gmail.com>
2024-09-01 14:02:50 +0530

    resp changes

--- Front-end/src/components/T_Doc.jsx
+++ Front-end/src/components/T_Doc.jsx
@@ -3,6 +3,7 @@ import logo from './img/favicon2.png';
 import down from './img/down.png';
 import search from './img/search.png';
 import share from './img/share.png';
+import dots from './img/dots.png';
 
 const T_Doc = () => {
     const [documents, setDocuments] = useState([
@@ -28,7 +29,7 @@ const T_Doc = () => {
 
                 <div className='md:ml-52 md:px-40 pt-32 justify-center flex flex-wrap items-center'>
                     {documents.map((doc) => (
-                        <div key={doc.id} className='md:w-full w-72 h-20 my-8 md:mx-10 mx-4 px-2 md:py-10 py-2 box flex justify-evenly items-center'>
+                        <div key={doc.id} className='md:w-full w-72 md:h-20 h-fit my-8 md:mx-10 mx-4 px-2 md:py-10 py-2 box flex justify-evenly items-center'>
                             <div className='hidden md:flex'>
                                 <lord-icon
                                     src="https://cdn.lordicon.com/wzwygmng.json"
@@ -46,17 +47,21 @@ const T_Doc = () => {
                                     delay="1500"
                                     stroke="bold"
                                     colors="primary:#3080e8,secondary:#a66037"
-                                    style={{ width: '20px', height: '50px' }}>
+                                    style={{ width: '35px', height: '50px' }}>
                                 </lord-icon>
                             </div>
-                            <div className='text ntext2'>
+                            <div className='text ntext1'>
                                 {doc.name}
                             </div>
-                            <div className='flex justify-center items-center gap-5 cursor-pointer'>
+                            <div className='hidden md:flex justify-center items-center gap-5 cursor-pointer'>
                                 <img src={search} alt="" />
                                 <img src={down} alt="" />
                                 <img src={share} alt="" />
                             </div>
+                            <div className='md:hidden flex'>
+                                <img src={dots} alt="" />
+                            </div>
+                           
                         </div>
                     ))}
                 </div>
