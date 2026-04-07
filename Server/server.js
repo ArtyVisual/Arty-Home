75dfa7dff9931b2aeff9bc9ffe0e398a21630532
abbas <abbasvajwana1@gmail.com>
2026-04-07 14:50:29 +0530

    new changes

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 app.options('*', cors(corsOptions)); // For all routes
 
-mongoose.connect("mongodb+srv://abbasvajwana1:AbbasMongo7@cluster1.0bhubyy.mongodb.net/rentDB", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Could not connect to MongoDB...", err));
 
